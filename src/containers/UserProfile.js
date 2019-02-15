531520dd01f614477b8edcfe763a3f68fd7e8c69
github username <github email address>
2019-02-14 23:43:23 -0600

    fixed organizers/attenders userprofile

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -4,7 +4,10 @@ import NavBar from './NavBar'
 export default class UserProfile extends Component {
 
     state = {
-        user: {} // note:this is the user of the page we're not, but NOT current user
+        user: {},
+        organizing: [],
+        attending: []
+        
     }
 
     componentDidMount(){
@@ -16,10 +19,66 @@ export default class UserProfile extends Component {
         })
             .then( res => res.json())
             .then( user => this.setState({ user }))
+            .then(() => this.fetchUserEvents())
+            .then(() => this.fetchArrangementsEvents())
+
+    }
+
+    fetchUserEvents() {
+        fetch(`http://localhost:3000/users/${this.props.match.params.id}/organizers`,{
+            method: 'GET',
+            headers:{
+                "Content-Type": 'application/json',
+                Authorization: `BEARER ${this.props.token}`
+            }
+        })
+        .then(res => res.json())
+            .then( organizing => this.setState({ organizing }))
+            .then(() => this.filterForOrganizing())
+    }
+
+    filterForOrganizing(){
+        let events = this.state.organizing[1]
+       let userevents = this.state.organizing[0].filter(ue => (
+            ue.user_id === this.state.user.id
+        ))
+        let arrayofEvents = userevents.map(ue => ue.event_id)
+        let events_organizing = arrayofEvents.map(id => events.filter(e => e.id === id))
+        this.setState({
+            organizing: events_organizing
+        })
+    }
+
+    fetchArrangementsEvents(){
+        fetch(`http://localhost:3000/users/${this.props.match.params.id}/attending`,{
+            method: 'GET',
+            headers:{
+                "Content-Type": 'application/json',
+                Authorization: `BEARER ${this.props.token}`
+            }
+        })
+        .then(res => res.json())
+            .then( attending => this.setState({ attending }))
+            .then(() => this.filterForAttending())
+
+    }
+
+    filterForAttending(){
+        let events = this.state.attending[1]
+       let userattending = this.state.attending[0].filter(ue => (
+            ue.user_id === this.state.user.id
+        ))
+        let arrayofEvents = userattending.map(ue => ue.event_id)
+        let events_attending = arrayofEvents.map(id => events.filter(e => e.id === id))
+        this.setState({
+            attending: events_attending
+        })
     }
 
     render() {
-       
+       console.log(this.state.organizing)
+       console.log(this.state.attending)
+
         const { name, email } = this.state.user
         return (
             <div>
